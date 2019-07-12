86752920428063d4413aa563d1012fe8e9ba8c09
Sergio Campos <morrodogg8@gmail.com>
2019-07-12 14:10:57 -0700

    finished project

--- p1.2/main.cpp
+++ p1.2/main.cpp
@@ -1,15 +1,34 @@
-//
-//  main.cpp
-//  p1.2
-//
-//  Created by Sergio campos on 5/19/19.
-//  Copyright © 2019 Sergio campos. All rights reserved.
-//
+/*
+ •• P1.2Write a program that prints out a message “Hello, my name is Hal!” Then, on a new line, the program should print the message “What is your name?” As in Exercise • P1.1, just use the following lines of code:
+ 
+ string user_name;
+ getline(cin, user_name);
+ Finally, the program should print the message “Hello, user name. I am glad to meet you!” To print the user name, simply use
+ 
+ cout << user_name;
+ As in Exercise • P1.1, you must place the line
+ 
+ #include <string>
+ before the main function.
+ 
+ Here is a typical program run. The user input is printed in color.
+ 
+ Hello, my name is Hal!
+ What is your name?
+ Dave
+ Hello, Dave. I am glad to meet you!
+ */
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main()
+{
+    cout << "Hello, my name is Hal!" << endl;
+    cout << "What is your name?: ";
+    string user_name;
+    getline(cin, user_name);
+    cout << "Hello " << user_name << " I am glad to meet you!" << endl;
 }
